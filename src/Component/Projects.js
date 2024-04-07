43b2fc757e99959403b46e8b651ee8ec1b7d6e5e
Pawan Raj Singh <122471520+pawanrajsingh2088@users.noreply.github.com>
2024-04-07 10:44:02 +0530

    updated

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -8,6 +8,7 @@ import text from "../Assets/textutils.png"
 import { Link } from 'react-router-dom'
 import Footer from './Footer'
 import pass from "../Assets/pass.png"
+import simon from "../Assets/simon.png"
 
 export default function Projects() {
   return (
@@ -67,6 +68,14 @@ export default function Projects() {
                   <h1 className="font-semibold text-4xl">Password Generator</h1>
                   <p className=" text-xl font-semibold text-slate-700">Skills Used:- ReactJs & Tailwind</p>
                   <img className=" border-4 border-slate-800 w-64 h-44 rounded-3xl" src={pass} alt="" />
+                </div>
+            </Link>
+            <Link target='_blank' to="https://pawanrajsingh2088.github.io/Simon-Game/">
+                <div className=" projects_box flex flex-col w-96 items-center border-4 my-5 border-red-500 p-4 gap-3 rounded-3xl bg-slate-200 hover:border-green-500 hover:scale-105 
+                  hover:shadow-2xl hover:shadow-orange-900">
+                  <h1 className="font-semibold text-4xl">Simon Game</h1>
+                  <p className=" text-xl font-semibold text-slate-700">Skills Used:- HTML, CSS & Javascript</p>
+                  <img className=" border-4 border-slate-800 w-64 h-44 rounded-3xl" src={simon} alt="" />
                 </div>
             </Link>
 
